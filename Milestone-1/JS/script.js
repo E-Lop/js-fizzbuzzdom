dd8e8df26415bb26b9a5b7f1b055e410275e3a18
E-Lop <edmondolopez@gmail.com>
2022-06-07 13:50:53 +0200

    milestone 1 completato

--- Milestone-1/JS/script.js
+++ Milestone-1/JS/script.js
@@ -1,5 +1,16 @@
 // ciclo for per numeri da 1 a 100
+const Fizz = 'Fizz';
+const Buzz = 'Buzz';
+const FizzBuzz = 'FizzBuzz';
 
 for (let i = 1; i <= 100; i++) {
-  console.log(i);
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(FizzBuzz);
+  } else if (i % 3 === 0) {
+    console.log(Fizz);
+  } else if (i % 5 === 0) {
+    console.log(Buzz);
+  } else {
+    console.log(i);
+  }
 }
