5945524efcd8a9271ff94d97cbf32b0cb3def471
E-Lop <edmondolopez@gmail.com>
2022-06-07 15:18:30 +0200

    milestone 3 finito

--- Milestone-3/JS/script.js
+++ Milestone-3/JS/script.js
@@ -5,31 +5,23 @@ Applica uno stile differente a seconda del valore dell'indice per i multipli di 
 per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5 
 (seguendo le regole della Milestone 1). */
 
-// ciclo for per numeri da 1 a 100
 const Fizz = 'Fizz';
 const Buzz = 'Buzz';
 const FizzBuzz = 'FizzBuzz';
+const squareContainer = document.querySelector('.square_container');
 
-/* for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    const newLi = `
-  <li>${FizzBuzz}</li>
-  `;
-    mainList.innerHTML += newLi;
+    const newBox = FizzBuzz;
+    squareContainer.innerHTML += `<div class="box salmon_bg">${newBox}</div>`;
   } else if (i % 3 === 0) {
-    const newLi = `
-  <li>${Fizz}</li>
-  `;
-    mainList.innerHTML += newLi;
+    const newBox = Fizz;
+    squareContainer.innerHTML += `<div class="box green_bg">${newBox}</div>`;
   } else if (i % 5 === 0) {
-    const newLi = `
-  <li>${Buzz}</li>
-  `;
-    mainList.innerHTML += newLi;
+    const newBox = Buzz;
+    squareContainer.innerHTML += `<div class="box yellow_bg">${newBox}</div>`;
   } else {
-    const newLi = `
-  <li>${i}</li>
-  `;
-    mainList.innerHTML += newLi;
+    const newBox = i;
+    squareContainer.innerHTML += `<div class="box blue_bg">${newBox}</div>`;
   }
-} */
+}
